dc6915745f705569e88e439418a77143916b63bb
Jared-Kyle Woo <JaredKyleWoo@MyMBA.local>
2018-11-03 17:34:46 -0400

    created nav bar

--- src/App.js
+++ src/App.js
@@ -2,26 +2,14 @@ import React, { Component } from 'react';
 import { Switch, Route, BrowserRouter as Router, Link } from 'react-router-dom';
 import HandyMan from './HandyMan'
 import TestComponent from './TestComponent'
+import {TextButtons} from './HandyMan'
 
 const App = () => {
   return ( 
     <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
-        </li>
-      </ul>
-
-      <hr />
-
-      <Route exact path="/" component={HandyMan} />
+     <div>
+    
+      <HandyMan />
       <Route path="/about"  component={TestComponent}/>
       
     </div>
