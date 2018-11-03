a7ec5bc8da706cbac5b4b6e06e21f1b0b5aae39d
jtdev <j.jthyriar@gmail.com>
2018-11-03 10:33:38 -0400

    removed react starter kit, clean slate.

---
+++ src/HandyMan.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HandyMan extends Component {
+  render() {
+    return (
+      <div className="App">
+        HandyMan
+      </div>
+    );
+  }
+}
+
+export default HandyMan;
