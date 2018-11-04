9db6fdd4df08691b2d5963cf08184398564598c8
Raymond H. Yang <raymond.yang@mail.mcgill.ca>
2018-11-04 11:23:22 -0500

    registrationforms

--- src/App.js
+++ src/App.js
@@ -5,32 +5,32 @@ import TestComponent from './TestComponent';
 import RegistrationForm from './RegistrationForm';
 
 const App = () => {
-  return ( 
+  return (
     <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
-        </li>
-        <li>
-          <Link to="/RegistrationForm">RegistrationForm</Link>
-        </li> 
-      </ul>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+          <li>
+            <Link to="/RegistrationForm">RegistrationForm</Link>
+          </li>
+        </ul>
 
-      <hr />
+        <hr />
 
-      <Route exact path="/" component={HandyMan} />
-      <Route path="/about"  component={TestComponent}/>
-      <Route path="/RegistrationForm"  component={RegistrationForm}/>
-      
-    </div>
-  </Router>
+        <Route exact path="/" component={HandyMan} />
+        <Route path="/about" component={TestComponent} />
+        <Route path="/RegistrationForm" component={RegistrationForm} />
+
+      </div>
+    </Router>
   )
 }
 
--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -6,61 +6,61 @@ import CustomerRegistration from "./CustomerRegistration"
 import HandymanRegistration from "./HandymanRegistration"
 
 const styles = theme => ({
-   
+
     button: {
-      marginLeft: theme.spacing.unit,
-      marginRight: theme.spacing.unit,
-      width: 200,
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+        width: 300,
     },
-  });
+});
 
 
 class RegistrationForm extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             type: ""
         }
-    this.switchStateToCustomer = this.switchStateToCustomer.bind(this)
-    this.switchStateToHandyman = this.switchStateToHandyman.bind(this)
+        this.switchStateToCustomer = this.switchStateToCustomer.bind(this)
+        this.switchStateToHandyman = this.switchStateToHandyman.bind(this)
     }
 
-    switchStateToCustomer(){
-        this.setState({type:"Customer"})
+    switchStateToCustomer() {
+        this.setState({ type: "Customer" })
     }
-    switchStateToHandyman(){
-        this.setState({type:"Handyman"})
+    switchStateToHandyman() {
+        this.setState({ type: "Handyman" })
     }
-    render(){    
+    render() {
         const { classes } = this.props;
-    return (
-    <div>
-      <Button
-        className={classes.button}
-        onClick={this.switchStateToHandyman}
-        disableRipple
-        size = "large"
-        variant="contained" 
-        color="primary" >
-        Handyman
+        return (
+            <div>
+                <Button
+                    className={classes.button}
+                    onClick={this.switchStateToHandyman}
+                    disableRipple
+                    size="large"
+                    variant="contained"
+                    color="primary" >
+                    Handyman
       </Button>
-      
-      <Button 
-        className={classes.button}
-        onClick={this.switchStateToCustomer}
-        disableRipple
-        size = "large"
-        variant="contained" 
-        color="primary" >
-        Customer
-      </Button>
-      
-    {this.state.type == "Customer" ? <CustomerRegistration/> : null}
-    {this.state.type == "Handyman" ? <HandymanRegistration/> : null}
 
-    </div>
-    );
+                <Button
+                    className={classes.button}
+                    onClick={this.switchStateToCustomer}
+                    disableRipple
+                    size="large"
+                    variant="contained"
+                    color="primary" >
+                    Customer
+      </Button>
+
+                {this.state.type == "Customer" ? <CustomerRegistration /> : null}
+                {this.state.type == "Handyman" ? <HandymanRegistration /> : null}
+
+            </div>
+        );
     }
 }
 
