a04a4695d3f7a68b3bef79894629899991478c69
Jared-Kyle Woo <JaredKyleWoo@MyMBA.local>
2018-11-04 10:55:40 -0500

    fixed conflict

--- src/App.js
+++ src/App.js
@@ -5,33 +5,13 @@ import TestComponent from './TestComponent'
 import {TextButtons} from './Navbar'
 import HandyManList from './HandyManList';
 import Home from './Home';
-import TestComponent from './TestComponent'
 import JobPosting from './JobPosting'
 
 const App = () => {
   return ( 
-    <Router>
-     <div>
-    
-      <Navbar />
+    <Router> 
     <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/handymen">Handymen</Link>
-        </li>
-        <li>
-          <Link to="/jobPosting">Job Postings</Link>
-        </li>
-      </ul>
-
-      <hr />
-
+      <Navbar />
       <Route exact path="/" component={Home} />
       <Route path="/about"  component={TestComponent}/>
       <Route path="/jobPosting" component={JobPosting}/>
