8a84f698139286d989376d2adf5695b962f56e31
Raymond H. Yang <raymond.yang@mail.mcgill.ca>
2018-11-04 13:43:13 -0500

    fixed conflicts

--- src/App.js
+++ src/App.js
@@ -1,38 +1,5 @@
 import React, { Component } from 'react';
 import { Switch, Route, BrowserRouter as Router, Link } from 'react-router-dom';
-<<<<<<< HEAD
-import HandyMan from './HandyMan';
-import TestComponent from './TestComponent';
-import RegistrationForm from './RegistrationForm';
-
-const App = () => {
-  return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
-          <li>
-            <Link to="/RegistrationForm">RegistrationForm</Link>
-          </li>
-        </ul>
-
-        <hr />
-
-        <Route exact path="/" component={HandyMan} />
-        <Route path="/about" component={TestComponent} />
-        <Route path="/RegistrationForm" component={RegistrationForm} />
-
-      </div>
-    </Router>
-=======
 import Navbar from './Navbar'
 import TestComponent from './TestComponent'
 import Registration from './Registration';
@@ -78,7 +45,6 @@ const App = () => {
         </div>
       </Router>
     </MuiThemeProvider>
->>>>>>> ed4b0ae60569133bad7e98b155ae60758658d408
   )
 }
 
