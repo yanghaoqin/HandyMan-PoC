797ffb0608fa3b38383d2b4d93d7835fdc98408c
jtdev <j.jthyriar@gmail.com>
2018-11-04 15:50:40 -0500

    replaced default search bar.

--- src/HandyManList.js
+++ src/HandyManList.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
-import {HandyMan} from './HandyMan';
+import { HandyMan } from './HandyMan';
+import Input from '@material-ui/core/Input';
+
 
 class HandyManList extends Component {
-    constructor(props)
-    {
+    constructor(props) {
         super(props)
         this.state = {
             searchString: '',
+            distance: 50,
             handyManList: [
-                { id:1, handyManName: 'Owen', dateCreated: new Date(), handyManTitle: 'Network Engineer', body: 'Network Engineer and currently single.', email: 'Owenli@gmail.com'},
-                { id:2, handyManName: 'Rameen', dateCreated: new Date(), handyManTitle: 'Software Engineer', body: 'React Dev.', email: 'RameenRastan@gmail.com'},
-                { id:3, handyManName: 'Jared', dateCreated: new Date(), handyManTitle: 'Coder', body: 'React Noob.', email: 'jaredWoo@live.ca'},
-                { id:4, handyManName: 'Wilson', dateCreated: new Date(), handyManTitle: 'Hacker', body: 'React noob', email: 'wilsonla@live.ca'}
+                { id: 1, handyManName: 'Owen', dateCreated: new Date(), handyManTitle: 'Network Engineer', body: 'Network Engineer and currently single.', email: 'Owenli@gmail.com', distance: 25 },
+                { id: 2, handyManName: 'Rameen', dateCreated: new Date(), handyManTitle: 'Software Engineer', body: 'React Dev.', email: 'RameenRastan@gmail.com', distance: 70 },
+                { id: 3, handyManName: 'Jared', dateCreated: new Date(), handyManTitle: 'Coder', body: 'React Noob.', email: 'jaredWoo@live.ca', distance: 100 },
+                { id: 4, handyManName: 'Wilson', dateCreated: new Date(), handyManTitle: 'Hacker', body: 'React noob', email: 'wilsonla@live.ca', distance: 50 }
 
             ],
             filterType: "name"
         };
         this.handleFilterSearch = this.handleFilterSearch.bind(this)
-        this.handleFilterType = this.handleFilterType.bind(this);
+        this.handleFilterType = this.handleFilterType.bind(this)
+        this.handleChangeDistance = this.handleChangeDistance.bind(this)
     }
     handleFilterSearch(e) {
         console.log("test")
@@ -31,30 +34,47 @@ class HandyManList extends Component {
         this.setState({ filterType: event.target.value })
     }
 
+    handleChangeDistance(event) {
+        this.setState({
+            distance: event.target.value
+        })
+    }
     render() {
         const filteredList = this.state.handyManList.filter(handyman => {
-                switch(this.state.filterType) {
-                    case "name" :
+            switch (this.state.filterType) {
+                case "name":
                     return handyman.handyManName.toUpperCase().includes(this.state.searchString.toUpperCase())
-                    case "title" :
+                case "title":
                     return handyman.handyManTitle.toUpperCase().includes(this.state.searchString.toUpperCase())
-                }
+                case "distance":
+                    return handyman.distance <= parseInt(this.state.distance)
+
+            }
         })
         return (
             <div>
-            <form>
-                <input id="filter" type="text" value={this.state.searchString} onChange={this.handleFilterSearch.bind(this)} />
-                <select onChange={this.handleFilterType} placeholder="Select a person" value={this.state.filterType} defaultValue={this.state.filterType}>
-                    <option value="name">HandyMan Name</option>
-                    <option value="title">HandyMan Title</option>
-                </select>
-            </form>
-            <h1>Available Handymen</h1>
-                    {filteredList.map(handyman => (
-                            <div key={handyman.id}>
-                                <HandyMan handyMan={handyman} />
-                            </div>
-                    ))}
+                <form>
+                    {this.state.filterType === "distance" ?
+                        <select onChange={this.handleChangeDistance.bind(this)} placeholder="Select a person" value={this.state.distance} defaultValue={this.state.distance}>
+                            <option value="50">50 km</option>
+                            <option value="100">100 km</option>
+                            <option value="500">500 km</option>
+                        </select>
+                        :
+                        <Input id="filter" type="text" value={this.state.searchString} onChange={this.handleFilterSearch.bind(this)} placeholder="Find HandyMan"></Input>
+                    }
+                    <select onChange={this.handleFilterType} placeholder="Select a person" value={this.state.filterType} defaultValue={this.state.filterType}>
+                        <option value="name">HandyMan Name</option>
+                        <option value="title">HandyMan Title</option>
+                        <option value="distance">HandyMan Distance</option>
+                    </select>
+                </form>
+                <h1>Available Handymen</h1>
+                {filteredList.map(handyman => (
+                    <div key={handyman.id}>
+                        <HandyMan handyMan={handyman} />
+                    </div>
+                ))}
             </div>
         )
     }
